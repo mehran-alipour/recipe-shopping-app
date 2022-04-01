07d9b8c38b6df24aa514eefd579dee7b511ffb33
mehran-alipour <mehran.a089@gmail.com>
2022-04-01 14:22:01 -0400

    the recipe-detail now is connected to recipe store

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -27,28 +27,42 @@ export class RecipeDetailComponent implements OnInit {
     private store: Store<fromApp.AppState>){}
 
   ngOnInit(): void {
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.itemId = +params['id'];
-        //this.recipe = this.recipeSrv.getRecipe(this.itemId)
-        this.store.select('recipes')
-          .pipe(
-            map(recState =>{
-              return recState.recipes.find((recipe, index)=>{
-                return index === this.itemId
-              })
-            })
-          ).subscribe(recipe => {
-            this.recipe = recipe
-          });
-      }
-    )
+    this.route.params.pipe(
+      map(param => {
+        return +param['id']
+      }),
+      switchMap(id => {
+        this.itemId = id;
+        return this.store.select('recipes')
+      }),
+      map(recipesState => {
+        return recipesState.recipes.find((rec, index)=>{
+          return index === this.itemId
+        })
+      })
+    ).subscribe(recipe => {
+      this.recipe = recipe
+    })
+    
+    
+    // this.route.params.subscribe(
+    //   (params: Params) => {
+    //     this.itemId = +params['id'];
+    //     this.store.select('recipes')
+    //       .pipe(
+    //         map(recState =>{
+    //           return recState.recipes.find((recipe, index)=>{
+    //             return index === this.itemId
+    //           })
+    //         })
+    //       ).subscribe(recipe => {
+    //         this.recipe = recipe
+    //       });
+    //   }
+    // )
   }
 
   toShoppingList(){
-    // for(let i in this.recipe.ingredints ){
-    //   this.shoppingListSrv.onAddItemToList(this.recipe.ingredints[i])
-    // }
     if(!this.itemsAddedToShoppingList){
       this.recipeSrv.addIngredients(this.recipe.ingredients);
       this.itemsAddedToShoppingList = true;
